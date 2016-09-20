0d4cfe376455e09731d9a28f34c2734404e1987b
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-09-21 02:10:18 +0800

    Test Cases for Goto

---
+++ goto_test_cases/goto25.c
@@ -0,0 +1,90 @@
+int main()
+{
+   int age;
+   int X;
+   int Y;
+   int Z;
+   X=0;
+   
+   Y=X+5;
+   
+   Z=2*Y;
+   X=2*age;
+   if(age>=18)
+   {
+     goto Vote;
+   }
+
+   while(X>=20)
+   {
+	 
+	while(X>=20)
+   	{
+
+		if(X>=20)
+   		{
+			X=X+1;
+   			age=X;
+	
+   		}
+   		else if(X>=40)
+   
+		{
+        
+			X=X+1;
+			age=age+1;
+   
+		}
+   
+		else
+   
+		{
+        
+			X=X+1;
+			age=X;
+	
+
+			if(X>=20)
+   			{
+				X=X+1;
+   				age=X;
+	
+   			}
+   			else if(X>=40)
+   
+			{
+        
+				X=X+1;
+				age=age+1;
+   
+			}
+   else
+   
+			{
+        
+				X=X+3;
+				Vote:
+				X=X+1;
+   				age=X;
+
+   			}
+
+
+   		}
+
+ 
+		Y=age+X;
+	 	   
+        	Z=age+Y;
+	}
+	
+   }
+
+  Y=age+X;
+	 	   
+  Z=age+Y;
+  
+  age=X;  
+
+
+}
