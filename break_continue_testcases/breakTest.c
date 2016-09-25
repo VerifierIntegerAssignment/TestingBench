8a345e32b8257dde8bfa5c603e5b49daf9f7a679
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-09-25 12:03:14 +0800

    Add files via upload

---
+++ break_continue_testcases/breakTest.c
@@ -0,0 +1,50 @@
+int main()
+{
+   int age;
+   int X;
+   
+   int Y;
+   
+   int Z;
+   X=0;
+   
+   Y=X+5;
+   
+   Z=2*Y;
+   if(age>=18)
+   {
+        Y=X+5;
+   
+   	Z=2*Y;
+   } 
+   X=2*age;
+   while(X>=20)
+   {
+	
+	Y=age+X;	
+	Y=X+5;
+   
+   	Z=2*Y;
+
+        if(age>=16)
+        {
+          break;
+        } 
+
+	Y=age+X;
+		   
+        Z=age+Y;
+	
+	if(age>=18)
+        {
+          break;
+        } 
+
+
+	
+   }
+
+   age=X;  
+
+
+}
---
+++ break_continue_testcases/breakTest1.c
@@ -0,0 +1,65 @@
+int main()
+{
+   int age;
+   int X;
+   
+   int Y;
+   
+   int Z;
+   X=0;
+   
+   Y=X+5;
+   
+   Z=2*Y;
+   if(age>=18)
+   {
+        Y=X+5;
+   
+   	Z=2*Y;
+   } 
+   X=2*age;
+   while(X>=20)
+   {
+	
+	Y=age+X;	
+	Y=X+5;
+   
+   	Z=2*Y;
+
+        if(age>=16)
+        {
+          break;
+        } 
+
+	Y=age+X;
+		   
+        Z=age+Y;
+
+	if(age>20)
+	{
+
+		if(age>=18)
+        	{
+          		age=X+Y;
+			continue;
+        	}
+		else{
+			X=X+1;
+			break;
+			
+		} 
+		X=Y+1;
+		age=Y+age;
+
+	}
+	
+	
+
+
+	
+   }
+
+   age=X;  
+
+
+}
