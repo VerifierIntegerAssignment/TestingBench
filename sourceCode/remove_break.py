dc153f77d5a05836236b9b21bf19b173f22ae808
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-09-25 12:07:46 +0800

    remove break

---
+++ sourceCode/remove_break.py
@@ -0,0 +1,198 @@
+#-----------------------------------------------------------------
+# remove_goto
+#
+# Removing Break,Continue  and  rewriting AST
+#
+# September 22 2016, Pritom Rajkhowa
+# Project: VIAP
+#-----------------------------------------------------------------
+import sys
+
+from pycparser import parse_file,c_parser, c_ast, c_generator
+
+
+
+
+new_variable={}
+
+break_count=0
+
+continue_count=0
+
+  
+    
+
+def remove_break(filename):
+    content=None
+    with open(filename) as f:
+        content = f.read()
+    content=content.replace('\r','')
+    text = r""" """+content
+    parser = c_parser.CParser()
+    ast = parser.parse(text)     
+    function_decl = ast.ext[0].decl
+    function_param = function_decl
+    function_body = ast.ext[0].body
+    statements=function_body.block_items
+    break_map={}
+    condition=None
+    break_count=0
+    statements=getBreakStmt(statements,break_map)
+    update_statements=[]
+    for var in new_variable.keys():
+    	temp=c_ast.Decl(name=var, quals=[], storage=[], funcspec=[], type=c_ast.TypeDecl(declname=var, quals=[], type=c_ast.IdentifierType(names=['int'])), init=c_ast.Constant(type='int', value='0'), bitsize=None)
+    	update_statements.append(temp)
+    for statement in statements:
+    	update_statements.append(statement)
+    function_body.block_items=update_statements
+    body_comp = c_ast.Compound(block_items=update_statements)
+    generator = c_generator.CGenerator()   
+    print(generator.visit(ast))
+    
+    
+
+	
+
+
+def getBreakStmt(statements,break_map):
+	update_statement1=[]
+	update_statement2=[]
+	flag=False
+	global break_count
+	global continue_count
+        global new_variable
+	for statement in statements:
+		if type(statement) is c_ast.If:
+			if flag==False:
+				break_map_temp={}
+				statement=getBreakStmtIf(statement,break_map_temp)
+				for e_break in break_map_temp.keys():
+					break_map[e_break]=break_map_temp[e_break]
+				update_statement1.append(statement)
+				if len(break_map_temp.keys())>0:
+					flag=True
+			else:
+				update_statement2.append(statement)
+		elif type(statement) is c_ast.While:
+			break_map_temp={}
+			new_block_items1=getBreakStmt(statement.stmt.block_items,break_map_temp)
+			new_block_items2=[]
+			new_condtion=statement.cond
+			if len(break_map_temp.keys())>0:
+				for var_name in break_map_temp.keys():
+                                        new_block_items2.append(c_ast.Assignment(op='=', lvalue=c_ast.ID(name=var_name), rvalue=c_ast.Constant(type='int', value='0')))
+					if break_map_temp[var_name]=='Break':
+						temp_new_condition=c_ast.BinaryOp(op='&&', left=new_condtion, right=c_ast.BinaryOp(op='==', left=c_ast.ID(name=var_name), right=c_ast.Constant(type='int', value='0')))
+						new_condtion=temp_new_condition
+					else:
+						temp_new_condition=c_ast.BinaryOp(op='||', left=new_condtion, right=c_ast.BinaryOp(op='==', left=c_ast.ID(name=var_name), right=c_ast.Constant(type='int', value='1')))
+						new_condtion=temp_new_condition
+
+                        for item in new_block_items1:
+                            new_block_items2.append(item)
+			if flag==False:
+				update_statement1.append(c_ast.While(cond=new_condtion, stmt=c_ast.Compound(block_items=new_block_items2)))
+			else:
+				update_statement2.append(c_ast.While(cond=new_condtion, stmt=c_ast.Compound(block_items=new_block_items2)))
+		elif type(statement) is c_ast.Break:
+                        break_count=break_count+1
+                        var_name='break_'+str(break_count)+'_flag'
+                        new_variable[var_name]=var_name
+                        update_statement1.append(c_ast.Assignment(op='=', lvalue=c_ast.ID(name=var_name), rvalue=c_ast.Constant(type='int', value='1')))
+			break_map[var_name]='Break'
+		elif type(statement) is c_ast.Continue:
+		        continue_count=continue_count+1
+		       	var_name='continue_'+str(continue_count)+'_flag'
+		        new_variable[var_name]=var_name
+		        update_statement1.append(c_ast.Assignment(op='=', lvalue=c_ast.ID(name=var_name), rvalue=c_ast.Constant(type='int', value='1')))
+			break_map[var_name]='Continue'
+		else:
+			if flag==False:
+				update_statement1.append(statement)
+			else:
+				update_statement2.append(statement)
+	if flag==True:
+		update_condition=None
+		for var_name in break_map.keys():
+			if update_condition is None:
+				update_condition=c_ast.BinaryOp(op='==', left=c_ast.ID(name=var_name), right=c_ast.Constant(type='int', value='0'))
+			else:
+				update_condition=c_ast.BinaryOp(op='&&', left=update_condition, right=c_ast.BinaryOp(op='==', left=c_ast.ID(name=var_name), right=c_ast.Constant(type='int', value='0')))
+		if len(update_statement2)>0:
+			update_statement1.append(c_ast.If(cond=update_condition, iftrue=c_ast.Compound(block_items=update_statement2), iffalse=None))
+		
+		return getBreakStmt(update_statement1,break_map)
+	else:
+		return update_statement1
+			
+		
+
+
+
+
+def getBreakStmtIf(statement,break_map):
+	new_iftrue=None
+	new_iffalse=None
+	global break_count
+	global new_variable
+	global continue_count
+	if type(statement) is c_ast.If:
+			
+		if type(statement.iftrue) is c_ast.Break:
+                        new_block_items=[]
+                        break_count=break_count+1
+                        var_name='continue_'+str(break_count)+'_flag'
+                        new_variable[var_name]=var_name
+                        new_block_items.append(c_ast.Assignment(op='=', lvalue=c_ast.ID(name=var_name), rvalue=c_ast.Constant(type='int', value='1')))
+			break_map[var_name]='Break'
+			new_iftrue=c_ast.Compound(block_items=new_block_items)
+		elif type(statement.iftrue) is c_ast.Continue:
+		        new_block_items=[]
+		        break_count=break_count+1
+		        var_name='break_'+str(continue_count)+'_flag'
+		        new_variable[var_name]=var_name
+		        new_block_items.append(c_ast.Assignment(op='=', lvalue=c_ast.ID(name=var_name), rvalue=c_ast.Constant(type='int', value='1')))
+			break_map[var_name]='Continue'
+			new_iftrue=c_ast.Compound(block_items=new_block_items)
+		elif type(statement.iftrue) is c_ast.Compound:
+			new_block_items=getBreakStmt(statement.iftrue.block_items,break_map)
+			new_iftrue=c_ast.Compound(block_items=new_block_items)
+			
+		if type(statement.iffalse) is c_ast.Break:
+                        new_block_items=[]
+                        break_count=break_count+1
+                        var_name='break_'+str(break_count)+'_flag'
+                        new_variable[var_name]=var_name
+                        new_block_items.append(c_ast.Assignment(op='=', lvalue=c_ast.ID(name=var_name), rvalue=c_ast.Constant(type='int', value='1')))
+			break_map[var_name]='Break'
+			new_iffalse=c_ast.Compound(block_items=new_block_items)
+		elif type(statement.iffalse) is c_ast.Continue:
+		        new_block_items=[]
+		        continue_count=continue_count+1
+		        var_name='continue_'+str(continue_count)+'_flag'
+		        new_variable[var_name]=var_name
+		        new_block_items.append(c_ast.Assignment(op='=', lvalue=c_ast.ID(name=var_name), rvalue=c_ast.Constant(type='int', value='1')))
+			break_map[var_name]='Continue'
+			new_iffalse=c_ast.Compound(block_items=new_block_items)	
+		elif type(statement.iffalse) is c_ast.Compound:
+			new_block_items=getBreakStmt(statement.iffalse.block_items,break_map)
+			new_iffalse=c_ast.Compound(block_items=new_block_items)
+		else:
+			if type(statement.iffalse) is c_ast.If:
+				new_iffalse=getBreakStmtIf(statement,break_map)
+	if new_iftrue is not None and new_iffalse is None:
+		return c_ast.If(cond=statement.cond, iftrue=new_iftrue, iffalse=None)
+	elif new_iftrue is not None and new_iffalse is not None:
+		return c_ast.If(cond=statement.cond, iftrue=new_iftrue, iffalse=new_iffalse)
+	elif new_iffalse is not None and type(new_iffalse) is c_ast.Compound:
+		return c_ast.If(cond=c_ast.UnaryOp(op='!', expr=statement.cond), iftrue=new_iffalse, iffalse=None)
+	elif new_iffalse is not None and type(new_iffalse) is c_ast.If:
+		return new_iffalse
+	else:
+		return None
+			 
+
+
+
+
+
