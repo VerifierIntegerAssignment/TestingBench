c4cde39a7c6b3160c521835fabd7037e0a5b1f94
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-09-27 14:58:00 +0800

    Function Test

---
+++ Function_Test/functionTest1.c
@@ -0,0 +1,51 @@
+int multiplyNumbers(int a,int b);
+int output;
+int main()
+{
+   int age;
+   int X;
+   
+   int Y;
+   
+   int Z;
+   X=0;
+   
+   Y=X+5;
+   
+   Z=2*Y;
+   multiplyNumbers(X,Y);
+   age=2*result;
+   multiplyNumbers(X,Y);
+
+   Y=X+5;
+   
+   Z=2*Y;
+   if(age>=X+testProgram(X,Y))
+   {
+	age=2*X;
+   }
+
+   
+ 
+
+
+}
+
+
+void multiplyNumbers(int a,int b)
+{
+	output = a*b;
+}
+
+int testProgram(int a,int b)
+{
+   int X;
+   
+   int Y;
+
+   int result;
+
+   X=a*b;
+   Y=2*b;
+   result=X+Y;
+}
---
+++ Function_Test/functionTest3.c
@@ -0,0 +1,61 @@
+int multiplyNumbers(int a,int b);
+int output;
+int main()
+{
+   int age;
+   int X;
+   
+   int Y;
+   
+   int Z;
+   X=0;
+   
+   Y=X+5;
+   
+   Z=2*Y;
+   multiplyNumbers(X,Y);
+   age=2*result;
+   multiplyNumbers(X,Y);
+
+   
+   Y=X+5;
+   
+   Z=2*Y;
+   
+   while(age>=testProgram(X,Y))
+   
+   {
+	
+     age=2*X;
+   
+     Y=X+2;
+   }
+
+   
+ 
+
+
+}
+
+
+void multiplyNumbers(int a,int b)
+{
+	output = a*b;
+}
+int testProgram(int a,int b)
+{
+   
+
+	int X;
+   
+        int Y;
+
+   
+        int result;
+
+        X=a*b;
+   
+        Y=2*b;
+   
+        result=X+Y;
+}
