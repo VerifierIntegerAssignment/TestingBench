b161046b536b220bd2013303334284ddfe55ccf9
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-09-30 17:31:40 +0800

    Add files via upload

---
+++ Integration_Test/integration_test.c
@@ -0,0 +1,32 @@
+int add(int a,int b);
+int product(int a,int b);
+int output;
+void main()
+{
+
+   int X=10;
+   
+   int Y=20;
+   
+   int Z;
+   
+
+   Z=add(X,Y)+product(X,Y);
+   
+
+}
+
+
+int add(int a,int b)
+{
+	int S;
+	S = a+b;
+	return S;
+}
+
+int product(int a,int b)
+{
+	int P;
+	P = a*b;
+	return P;
+}
