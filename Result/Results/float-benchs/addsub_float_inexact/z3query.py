0f562285e9d3d682bb15557209827285571a5742
VerifierIntegerAssignment <prajkhowa@ust.hk>
2017-02-24 09:23:41 +0800

    Add files via upload

---
+++ Result/Results/float-benchs/addsub_float_inexact/z3query.py
@@ -0,0 +1,54 @@
+from z3 import *
+set_param(proof=True)
+_p1=Int('_p1')
+_p2=Int('_p2')
+_n=Int('_n')
+arraySort = DeclareSort('arraySort')
+_f=Function('_f',IntSort(),IntSort())
+_1_PROVE=Int('_1_PROVE')
+RET=Int('RET')
+r=Real('r')
+y=Real('y')
+x=Real('x')
+z=Real('z')
+r1=Real('r1')
+_1_PROVE1=Int('_1_PROVE1')
+RET1=Int('RET1')
+y1=Real('y1')
+x1=Real('x1')
+z1=Real('z1')
+main=Int('main')
+__VERIFIER_assert=Function('__VERIFIER_assert',RealSort(),IntSort())
+_s=Solver()
+_s.add(ForAll([_n],Implies(_n>=0, _f(_n)==_n)))
+_s.set("timeout",60000)
+_s.add(y1 == 100000001.000000)
+_s.add(x1 == 100000000.000000)
+_s.add(r1 == 2.00000000000000)
+_s.add(z1 == 99999999.0000000)
+_s.add(main == 0)
+_s.add(Not((2.00000000000000==0.0)))
+if sat==_s.check():
+	print "Counter Example"
+	print _s.model()
+	witnessXmlStr=['<?xml version="1.0" encoding="UTF-8" standalone="no"?><graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><key attr.name="isEntryNode" attr.type="boolean" for="node" id="entry"><default>false</default></key><key attr.name="isViolationNode" attr.type="boolean" for="node" id="violation"><default>false</default></key><key attr.name="witness-type" attr.type="string" for="graph" id="witness-type"/><key attr.name="sourcecodelang" attr.type="string" for="graph" id="sourcecodelang"/><key attr.name="producer" attr.type="string" for="graph" id="producer"/><key attr.name="specification" attr.type="string" for="graph" id="specification"/><key attr.name="programFile" attr.type="string" for="graph" id="programfile"/><key attr.name="programHash" attr.type="string" for="graph" id="programhash"/><key attr.name="memoryModel" attr.type="string" for="graph" id="memorymodel"/><key attr.name="architecture" attr.type="string" for="graph" id="architecture"/><key attr.name="startline" attr.type="int" for="edge" id="startline"/><key attr.name="assumption" attr.type="string" for="edge" id="assumption"/><key attr.name="assumption.scope" attr.type="string" for="edge" id="assumption.scope"/><key attr.name="assumption.resultfunction" attr.type="string" for="edge" id="assumption.resultfunction"/><graph edgedefault="directed"><data key="witness-type">violation_witness</data><data key="sourcecodelang">C</data><data key="producer">CPAchecker 1.6.1-svn</data><data key="specification">CHECK( init(main()), LTL(G ! call(__VERIFIER_error())) )</data><data key="programfile">sv-benchmarks-master/sv-benchmarks-master/c/float-benchs/addsub_float_inexact.c</data><data key="programhash">1776ed2413d170f227b69d8c79ba700d31db6f75</data><data key="memorymodel">precise</data><data key="architecture">32bit</data><node id="entry"><data key="entry">true</data></node><node id="error"><data key="violation">true</data></node><edge source="entry" target="error">', '<data key="assumption.scope">main</data><data key="assumption.resultfunction">__VERIFIER_nondet_int</data></edge></graph></graphml>', 'main', 'sv-benchmarks-master/sv-benchmarks-master/c/float-benchs/addsub_float_inexact.c']
+	middle=''
+	for element in _s.model():
+		if str(element)==witnessXmlStr[2]:
+			middle+='<data key="assumption">'+'\\'+'result=='+str(_s.model()[element])+'</data>'
+	file = open(witnessXmlStr[3]+'_witness.graphml', 'w')
+	file.write(witnessXmlStr[0]+middle+witnessXmlStr[1])
+	file.close()
+elif unsat==_s.check():
+	_s.check()
+	if os.path.isfile('j2llogs.logs'):
+		file = open('j2llogs.logs', 'a')
+		file.write("\n**************\nProof Details\n**************\n"+str(_s.proof().children())+"\n")
+		file.close()
+	else:
+		file = open('j2llogs.logs', 'w')
+		file.write("\n**************\nProof Details\n**************\n"+str(_s.proof().children())+"\n")
+		file.close()
+	print "Successfully Proved"
+else:
+	print "Failed To Prove"
